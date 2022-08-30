3fb0f49cd1cfce1011ad8066b999354cefe47586
Domitrius Clark <domitriusaclark@gmail.com>
2022-08-30 01:56:30 -0400

    Override theme colors and add needed gradient classes. Setup new default fonts and added the logo pass through.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
+  theme: {
+    colors: {
+      "green-accent": "#00ffb2",
+      "primary-blue": "#161a2b",
+      "primary-text": "white",
+      "discord-blue": "#0066ff",
+      "white": "#ffffff",
+      "pink": "#E7017A",
+      "orange": "#DF4A1F",
+    }
+  },
   content: [
     "./src/**/*.{njk,md,html}",
     "./src/_includes/**/*.njk"
   ]
-}
+};
